b5b43667b15da198394bd6d7d62e5b9aebcb7443
eWheat <rbl4a@icloud.com>
2021-01-14 19:54:24 +0500

    Initial commit

---
+++ src/main/java/com/space/model/Ship.java
@@ -0,0 +1,138 @@
+package com.space.model;
+import javax.persistence.*;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Entity
+@Table(name = "ship")
+public class Ship {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "planet")
+    private String planet;
+
+    @Column(name = "shipType")
+    private ShipType shipType;
+
+    @Column(name = "prodDate")
+    private Date prodDate;
+
+    @Column(name = "isUsed")
+    private Boolean isUsed;
+
+    @Column(name = "speed")
+    private Double speed;
+
+    @Column(name = "crewSize")
+    private Integer crewSize;
+
+    @Column(name = "rating")
+    private Double rating;
+
+    private final DateFormat dateFormat = new SimpleDateFormat("yyyy");
+    private Date currentDate;
+    {
+        try {
+            currentDate = dateFormat.parse("3019");
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Ship(Long id, String name, String planet, ShipType shipType, Date prodDate, Boolean isUsed, Double speed, Integer crewSize) {
+        this.id = id;
+        this.name = name;
+        this.planet = planet;
+        this.shipType = shipType;
+        this.prodDate = prodDate;
+        this.isUsed = isUsed;
+        this.speed = speed;
+        this.crewSize = crewSize;
+    }
+
+    public Ship(Long id, String name, String planet, ShipType shipType, Date prodDate, Double speed, Integer crewSize) {
+        this.id = id;
+        this.name = name;
+        this.planet = planet;
+        this.shipType = shipType;
+        this.prodDate = prodDate;
+        this.isUsed = false;
+        this.speed = speed;
+        this.crewSize = crewSize;
+    }
+
+    public Ship() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPlanet() {
+        return planet;
+    }
+
+    public void setPlanet(String planet) {
+        this.planet = planet;
+    }
+
+    public ShipType getShipType() {
+        return shipType;
+    }
+
+    public void setShipType(ShipType shipType) {
+        this.shipType = shipType;
+    }
+
+    public Date getProdDate() {
+        return prodDate;
+    }
+
+    public void setProdDate(Date prodDate) {
+        this.prodDate = prodDate;
+    }
+
+    public Boolean getUsed() {
+        return isUsed;
+    }
+
+    public void setUsed(Boolean used) {
+        isUsed = used;
+    }
+
+    public Double getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(Double speed) {
+        this.speed = speed;
+    }
+
+    public Integer getCrewSize() {
+        return crewSize;
+    }
+
+    public void setCrewSize(Integer crewSize) {
+        this.crewSize = crewSize;
+    }
+
+    public Double getRating() {
+        return rating;
+    }
+
+    public void setRating(Double rating) {
+        this.rating = rating;
+    }
+}
